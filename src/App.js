1d7226c671344cd624f39cccf4fffa1a1225d66c
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2019-11-19 20:19:05 -0500

    navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NASAData from "./components/NASAList";
+import NavBar from "./components/navBar/navbar";
 
 function App() {
   return (
     <div className="App">
       <NASAData />
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/navBar/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
